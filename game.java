92c218904316dc1613cacac3a5ca862500605f3b
MagentaMagenta <119783546+FrinceNacion@users.noreply.github.com>
2023-01-21 12:53:18 +0800

    Update game.java

--- game.java
+++ game.java
@@ -7,18 +7,17 @@ import java.util.Scanner;
             int answerCounter = 0;
             String questionBank[][]={{"What's your name?" , "Miguel"}, {"How old are you?" , "16"}, {"Name of my dog?" , "Kobe"}, {"Name of my girlfriend", "Janelle"}, {"Favorite color of mine?", "Bluegreen"}};
             String answerBank[]= new String[questionBank.length];
-            for(int ans = 0; ans<questionBank.length; ans++){
-                System.out.println("questionBank[ans] [0]");
-                answerBank[ans]= sc.nextLine();
 
                 for(int pointer=0; pointer<answerBank.length; pointer++){
+                    System.out.println(questionBank[pointer] [0]);
+                    answerBank[pointer]= sc.nextLine();
                     if(questionBank[pointer][1].equalsIgnoreCase(answerBank[pointer])){
                         answerCounter ++;
                         System.out.println("Goodjob!");                   
                     }
-                    System.out.println("answerCounter");
+                    System.out.println("Score: "+answerCounter);
                 }
             }
-        }
+        
 
     }
